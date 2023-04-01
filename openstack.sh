2d7316946ae1e73ebe92452cd7521e4ad5033561
openstack-rap <128293768+openstack-rap@users.noreply.github.com>
2023-04-01 15:53:19 +0530

    Create openstack.sh

---
+++ openstack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+sudo -u stack bash /snap/openstack/x1/stack.sh
