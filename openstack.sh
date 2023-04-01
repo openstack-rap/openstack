3d835df3410512b24fce68a7d1f15168ba98f8cd
openstack-rap <128293768+openstack-rap@users.noreply.github.com>
2023-04-01 16:20:21 +0530

    Update openstack.sh

--- openstack.sh
+++ openstack.sh
@@ -3,4 +3,6 @@ sudo useradd -s /bin/bash -d /opt/stack -m stack
 sudo chmod +x /opt/stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 sudo chown stack:stack /snap/opestack/x1
+echo "done chown"
 sudo -u stack bash /snap/openstack/x1/stack.sh
+echo "done stack.sh"
